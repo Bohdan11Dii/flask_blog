2e4925ac0e653396a7213ff5a83726b3d08f78c3
Bohdan11Dii <bodyabrovdiy@gmail.com>
2025-09-02 20:05:00 +0200

    Create flask file and add one more routes about

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+@app.route('/about')
+def about():  # put application's code here
+    return 'Hello About!'
+
+if __name__ == '__main__':
+    app.run()
