54b7fca8a3f34b3efdba61ffd7cb8f576349798f
Bohdan11Dii <bodyabrovdiy@gmail.com>
2025-09-06 12:21:12 +0200

    Add url_for

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Bohdan Isaiovych',
+        'title': 'My First Post',
+        'content': 'My first post content',
+        'date_posted': 'April 10, 1999',
+    },
+    {
+        'author': 'James Adams',
+        'title': 'My Second Post',
+        'content': 'My second post content',
+        'date_posted': 'April 29, 2000',
+    }
+]
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+
+    return render_template("home/index.html", posts=posts, subtitle="This is my Blog")
 
 
-@app.route('/about')
+@app.route('/about/')
 def about():  # put application's code here
-    return 'Hello About!'
+    return render_template("about/about.html")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
